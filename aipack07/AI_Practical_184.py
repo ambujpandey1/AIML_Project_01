d24b53b33f86e0df5f5c04f3e99294be3a47d797
Ambuj <ambujp863@gmail.com>
2025-08-13 12:16:40 +0530

    sdfgh

--- aipack07/AI_Practical_184.py
+++ aipack07/AI_Practical_184.py
@@ -15,7 +15,7 @@ plt.title("Insurance data")
 plt.xlabel("Age")
 plt.ylabel("Insured or not")
 plt.scatter(x,y,color='r')
-# plt.show()
+plt.show()
 
 n = np.size(x)
 m_x = np.mean(x)
@@ -24,21 +24,41 @@ m_y = np.mean(y)
 ss_xy = np.sum(x * y) - n * m_x * m_y
 ss_xx = np.sum(x * x) - n * m_x * m_x
 
-m = ss_xx / ss_xy
+m = ss_xy / ss_xx
 c = m_y - m * m_x
 
 print("Value of m: ",m)
 print("Value of c: ",c)
 
-print("\nModel :  y=",m,"x+",c)
+print("\nModel :  y = ", m ," * x + ", c)
 
 plt.scatter(x, y, color='r', marker='o', s=30)
 y_pred = c + m * x
 
 plt.scatter(x, y_pred, color='g', marker='o', s=60)
 plt.plot(x, y_pred, color='b')
-
 plt.xlabel("--------------X--------------->")
 plt.ylabel("--------------Y--------------->")
-
 plt.show()
+
+
+import  math
+
+def get_sigmoid(z):
+    return 1/(1+np.power(math.e,-z))
+
+
+
+
+
+
+plt.title("Insurance Data")
+plt.xlabel("Age")
+plt.ylabel("Insured or not")
+plt.scatter(x,y,color='b',s=150)
+plt.plot(x,get_sigmoid(y_pred),'ro-')
+plt.show()
+
+
+
+
